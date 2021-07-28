e7274335c330df6907b55d848b89ae17983ac267
Мусин Михаил <musin.mihail@gmail.com>
2021-07-28 16:26:00 +0300

    level 12

---
+++ Assets/LevelSea/Level12/Level12Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level12Item : MonoBehaviour
+{
+    public Vector2 _scale;
+    void Start()
+    {
+        _scale = transform.localScale;
+    }
+}
--- Assets/LevelSea/Level12/Level12Mouse.cs
+++ Assets/LevelSea/Level12/Level12Mouse.cs
@@ -11,10 +11,12 @@ public class Level12Mouse : MonoBehaviour
             Level12.WaitHint = 1;
             GetComponent<BoxCollider2D>().enabled = false;
             GetComponent<SpriteRenderer>().sortingOrder = 12;
-            GetComponent<Animator>().Play("Move");
-            Invoke("Particle", 0.0f);
-            StartCoroutine(Particle2(Level12.count));
-            Level12.nextFigure();
+            Particle();
+            StartCoroutine(MoveItem(gameObject, Level12.AllTargetStatic[Level12.count]));
+            StartCoroutine(ScaleItem(gameObject, Level12.AllTargetStatic[Level12.count]));
+            
+            // GetComponent<Animator>().Play("Move");
+            // Invoke("Particle", 0.0f);
         }
     }
     void Particle()
@@ -24,7 +26,7 @@ public class Level12Mouse : MonoBehaviour
     }
     IEnumerator Particle2(int count)
     {
-        yield return new WaitForSeconds(1.5f);
+        // yield return new WaitForSeconds(1.5f);
         Instantiate(Resources.Load<ParticleSystem>("ParticleSrarsLevel11"), transform.position, Quaternion.Euler(-90,0,0));
         Level12.AllTargetStatic[count].GetComponent<SpriteRenderer>().enabled = false;
         if(WinBobbles.Victory == 1)
@@ -32,7 +34,8 @@ public class Level12Mouse : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
             foreach (var item in Level12.AllItemStatic)
             {
-                item.GetComponent<Animator>().Play("Scale");
+                
+                StartCoroutine(Win(item));
                 yield return new WaitForSeconds(0.02f);
             }
             WinBobbles.Victory--;
@@ -40,6 +43,41 @@ public class Level12Mouse : MonoBehaviour
         else
         {
             WinBobbles.Victory--;
+        }
+    }
+    IEnumerator MoveItem(GameObject item, GameObject target)
+    {
+        target.GetComponent<Animator>().enabled = false;
+        target.transform.localScale = target.GetComponent<Level12Item>()._scale;
+        while(item.transform.position != target.transform.position)
+        {
+            item.transform.position = Vector2.MoveTowards(item.transform.position, target.transform.position, 0.1f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        StartCoroutine(Particle2(Level12.count));
+        Level12.nextFigure();
+    }
+    IEnumerator ScaleItem(GameObject item, GameObject target)
+    {
+        while(item.transform.localScale != target.transform.localScale)
+        {
+            item.transform.localScale = Vector2.MoveTowards(item.transform.localScale, target.transform.localScale, 0.5f);
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+    IEnumerator Win(GameObject item)
+    {
+        var normalScale = item.transform.localScale;
+        var bigScale = item.transform.localScale*1.5f;
+        while(item.transform.localScale != bigScale)
+        {
+            item.transform.localScale = Vector2.MoveTowards(item.transform.localScale, bigScale, 0.5f);
+            yield return new WaitForSeconds(0.003f);
+        }
+        while(item.transform.localScale != normalScale)
+        {
+            item.transform.localScale = Vector2.MoveTowards(item.transform.localScale, normalScale, 0.5f);
+            yield return new WaitForSeconds(0.003f);
         }
     }
 }
